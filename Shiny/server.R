d864c8786549399b59cc785182deda7e267188c3
kweiss2 <kevin.weiss@emory.edu>
2017-02-06 15:51:21 -0500

    2/6 changes

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,56 +1,91 @@
 library(shiny)
 library(shinydashboard)
-library(EpiModel)
 library(deSolve)
-source("fx.R")
-
-# https://github.com/statnet/EpiModel/blob/master/R/dcm.R
-# https://github.com/statnet/EpiModel/blob/master/R/dcm.mods.R
-# https://github.com/statnet/EpiModelResearch/blob/master/shiny/epidcmDG/ui.R
-# *******https://github.com/statnet/EpiModelResearch/blob/master/shiny/epidcmDG/server.R
-# http://desolve.r-forge.r-project.org/
-# https://www.r-bloggers.com/sir-model-of-epidemics/
-# http://rstudio-pubs-static.s3.amazonaws.com/6852_c59c5a2e8ea3456abbeb017185de603e.html
-
-
-
 
 shinyServer(function(input, output, session) {
 
-    # Reactive parameters    
-    parms <- reactive({list(
+    # Params values
+    params <- reactive({
         
+        list(
         # Input parameters
-        N.tot <- input$N.tot, 
-        prop.high <- input$prop.high,
-        prop.low <- 1 - input$prop.high,
-        c.mean = input$c.mean,
-        c.low = input$c.low,
+        c.mean = input$c.mean / 50,
+        c.low = input$c.low / 50,
         rho.high = input$rho.high,
         rho.low = input$rho.low,
-        brate = input$brate,
-        muS.high = input$muS.high,
-        muS.low = input$muS.low,
-        muI.high = input$muI.high,
-        muI.low = input$muI.low,
-        Q = input$Q,
-  
-        # Initial starting values
-        S.high = N.tot*prop.high - 1,
+        brate = input$brate / 50,
+        muS.high = input$muS.high / 50,
+        muS.low = input$muS.low / 50,
+        muI.high = input$muI.high / 50,
+        muI.low = input$muI.low / 50,
+        Q = input$Q)
+    })
+    
+    t0 <- c(
+        S.high = (prop.high)*(N.tot) - 1,
         I.high = 1,
-        S.low = N.tot*prop.low - 1,
+        S.low = (prop.low)*(N.tot) - 1,
         I.low = 1
     )
-
+    
     dt <- seq(1, 100, 0.02)
     
-    scenarioA <- Qmod(t = dt, parms = parms, t0 = t0)
-    #df1 <- data.frame(ode(y=t0, times=dt, func=Qmod, parms=params, method='rk4'))
+    ## New module
+    Qmod <- function(t, t0, parms) {
+        with(as.list(c(t0, parms)), {
+            
+            ## Dynamic Calculations ##
+            
+            # Popsize
+            N.high <- S.high + I.high
+            N.low <- S.low + I.low
+            N <- N.high + N.low
+            prev <- (I.high + I.low)/N
+            
+            # Contact rates
+            c.high <- (c.mean*N - c.low*N.low)/N.high
+            
+            # mixing matrix calculations based on Q
+            g.hh <- ((c.high*N.high) + (Q*c.low*N.low)) / ((c.high*N.high) + (c.low*N.low))
+            g.lh <- 1 - g.hh
+            g.hl <- (1 - g.hh) * ((c.high*N.high) / (c.low*N.low))
+            g.ll <- 1 - g.hl
+            
+            # prob that p is infected
+            p.high <- (g.hh*I.high/N.high) + (g.lh*I.low/N.low)
+            p.low <- (g.ll*I.low/N.low) + (g.hl*I.high/N.high)
+            
+            # lambda - force of infection
+            lambda.high <- rho.high*c.high*p.high
+            lambda.low <- rho.low*c.low*p.low
+            
+            ## Differential Equations ##
+            dS.high <- brate*prop.high*N.tot - lambda.high*S.high - muS.high*S.high 
+            dI.high <- lambda.high*S.high - muI.high*I.high
+            
+            dS.low <- brate*prop.low*N.tot - lambda.low*S.low - muS.low*S.low
+            dI.low <- lambda.low*S.low - muI.low*I.low
+            
+            
+            ## Output ##
+            list(c(dS.high, dI.high,
+                   dS.low, dI.low),
+                 N = N,
+                 prev = prev)
+            
+        })
+    }
+    
+    # Reactive parameters
+    ## ------------------------------------------------------------------------
+    
+    scenarioA <- data.frame(ode(y = t0, times = dt, func = Qmod, parms = params, method = "rk4"))
+
     output$a_Plot <- renderPlot({
         par(mfrow = c(1,1), mgp = c(2,1,0))
-        plot(datA = scenarioA, y = "prev", xlab = "Time",
-             ylab = "Prevalence")
+        plot(datA = scenarioA, y = scenarioA$prev, type = 'l', xlab = "Time",
+             ylab = "Prevalence", lwd = 2, ylim = c(0, 1))
          title("Prevalence of HIV")
         })
     })
-})
+
--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,8 +1,6 @@
 library(shiny)
 library(shinydashboard)
-library(wesanderson)
 library(deSolve)
-source("fx.R")
 
 # Define UI for application that draws a histogram
 shinyUI(dashboardPage(
@@ -33,45 +31,41 @@ shinyUI(dashboardPage(
                              ),
                         
                         # Input parameters
-                        column(width = 4,
+                        column(width = 2,
                                        box(width = NULL,
                                            title = "Model Parameters" , status = "success", solidHeader = TRUE,
-                                           selectInput(inputId = "N.tot", label = "Initial Population Size",
-                                                       choices = c("1000000", "5000000", "10000000", "20000000")),
-                                           selectInput(inputId = "brate", label = "Birth Rate",
-                                                       choices = c("0.5%", "1.0%", "1.5%", "2.0")),
-                                           selectInput(inputId = "c.low", label = "Contact rate for low-risk group",
-                                                       choices = c("26% (Paper Model)",
-                                                                   "20%", "15%", "10%")),
-                                           selectInput(inputId = "c.mean", label = "Mean contact rate",
-                                                       choices = c("26% (Paper Model)",
-                                                                   "20%", "15%", "10%")),
-                                           selectInput(inputId = "rho.high", label = "Transmission parameter for high-risk group",
-                                                       choices = c("26% (Paper Model)",
-                                                                   "20%", "15%", "10%")),
-                                           selectInput(inputId = "rho.low", label = "Transmission parameter for high-risk group",
-                                                       choices = c("26% (Paper Model)",
-                                                                   "20%", "15%", "10%")),
-                                           selectInput(inputId = "muS.low", label = "Death rate for low-risk group w/o Disease",
-                                                       choices = c("26% (Paper Model)",
-                                                                   "20%", "15%", "10%")),
-                                           selectInput(inputId = "muI.low", label = "Death rate for low-risk group w Disease",
-                                                       choices = c("26% (Paper Model)",
-                                                                   "20%", "15%", "10%")),
-                                           selectInput(inputId = "muS.high", label = "Death rate for high-risk group w/o Disease",
-                                                       choices = c("26% (Paper Model)",
-                                                       "20%", "15%", "10%")),
-                                           selectInput(inputId = "muI.high", label = "Death rate for high-risk group w Disease",
-                                                       choices = c("26% (Paper Model)",
-                                                                   "20%", "15%", "10%")),
-                                           selectInput(inputId = "Q", label = "Assortative mixing parameter",
-                                                       choices = c("26% (Paper Model)",
-                                                                   "20%", "15%", "10%")),
-                                           sliderInput(inputId = "prop.high", label = "Proportion in high-risk group",
-                                                       min = 0, max = 1, value = 0.02)
+                                           sliderInput(inputId = "N.tot", label = "Initial Population Size",
+                                                       min = 1000000, max = 20000000, step = 1000000, value = 2000000), 
+                                           sliderInput(inputId = "brate", label = "Birth Rate (%/year)",
+                                                       min = 0, max = 3, value = 0.5, step = 0.5),
+                                           sliderInput(inputId = "c.low", label = "Contact rate for Group 1 (partners / year)",
+                                                       min = 0, max = 30, value = 15, step = 1),
+                                           sliderInput(inputId = "c.mean", label = "Mean contact rate (partners / year)",
+                                                       min = 0, max = 30, value = 15, step = 1),
+                                           sliderInput(inputId = "rho.high", label = "Transmission parameter for Group 1",
+                                                       min = 0, max = 1, value = 0.20, step = 0.05),
+                                           sliderInput(inputId = "rho.low", label = "Transmission parameter for Group 2",
+                                                       min = 0, max = 1, value = 0.20, step = 0.05)
                                        )
-                            )
-            
+                            ),
+                        
+                        column(width = 2,
+                               box(width = NULL,
+                                   title = "Model Parameters" , status = "success", solidHeader = TRUE,
+                                   sliderInput(inputId = "muS.low", label = "Death rate for low-risk group w/o Disease (% / year)",
+                                               min = 0, max = 3, value = 0.5, step = 0.5),
+                                   sliderInput(inputId = "muI.low", label = "Death rate for low-risk group w Disease",
+                                               min = 0, max = 3, value = 0.5, step = 0.5),
+                                   sliderInput(inputId = "muS.high", label = "Death rate for high-risk group w/o Disease",
+                                               min = 0, max = 3, value = 0.5, step = 0.5),
+                                   sliderInput(inputId = "muI.high", label = "Death rate for high-risk group w Disease",
+                                               min = 0, max = 3, value = 0.5, step = 0.5),
+                                   sliderInput(inputId = "Q", label = "Assortative mixing parameter",
+                                               min = 0, max = 1, value = 0.02, step = 0.01),
+                                   sliderInput(inputId = "prop.high", label = "Proportion in high-risk group",
+                                               min = 0, max = 1, value = 0.02, step = 0.01)
+                               )
+                        )
                         )
                 )
             )
