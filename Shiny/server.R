35b21a424cf54ad1bf1257fc341d955d7c503cd5
kweiss2 <kevin.weiss@emory.edu>
2017-02-09 13:00:44 -0500

    Updates to EpiModel structure

--- Shiny/server.R
+++ Shiny/server.R
@@ -3,44 +3,50 @@ library(shinydashboard)
 library(deSolve)
 source("fx.R")
 
-shinyServer(function(input, output, session) {
-
-    # Inputs
-    params <- reactive({
-        list(
-            # Input parameters
-            prop.high = input$prop.high,
-            prop.low = 1 - input$prop.high,
-            N.tot = input$N.tot,
-            c.mean = input$c.mean / 50,
-            c.low = input$c.low / 50,
-            rho.high = input$rho.high,
-            rho.low = input$rho.low,
-            brate = input$b.rate / 50,
-            muS.high = input$muS.high / 50,
-            muS.low = input$muS.low / 50,
-            muI.high = input$muI.high / 50,
-            muI.low = input$muI.low / 50,
-            Q = input$Q
-        )
+shinyServer(function(input, output) {
+    
+    ## Main reactive functions
+    param <- reactive({
+        param.dcm(c.mean = input$c.mean / 50,
+                  c.low = input$c.low / 50,
+                  rho.high = input$rho.high,
+                  rho.low = input$rho.low,
+                  brate = input$b.rate / 50,
+                  muS.high = input$muS.high / 50,
+                  muS.low = input$muS.low / 50,
+                  muI.high = input$muI.high / 50,
+                  muI.low = input$muI.low / 50,
+                  Q = input$Q)
     })
-    
-    t0 <- c(S.high = (prop.high)*(N.tot) - 1,
-            I.high = 1,
-            S.low = (prop.low)*(N.tot) - 1,
-            I.low = 1
-    )
-    
-    dt <- seq(1, 100, 0.02)
-    
+    init <- reactive({
+            init.dcm(prop.high = input$prop.high,
+                     prop.low = 1 - input$prop.high,
+                     N.tot = input$N.tot,
+                     S.high = prop.high*N.tot - 1,
+                     I.high = 1,
+                     S.low = prop.low*N.tot - 1,
+                     I.low = 1
+                      )
+    })
+    control <- reactive({
+        control.dcm(type = input$modtype,
+                    nsteps = 100 / 0.02,
+                    dt = 0.02,
+                    verbose = FALSE,
+                    odemethod = "rk4")
+    })
+    mod <- reactive({
+        input$runMod
+        isolate(dcm(param(), init(), control()))
+    })
     ## ------------------------------------------------------------------------
     # Output plot
     
-    scenarioA <- data.frame(ode(y = t0, times = dt, func = Qmod, parms = params, method = "rk4"))
+    df <- as.data.frame(mod())
 
     output$a_Plot <- renderPlot({
         par(mfrow = c(1,1), mgp = c(2,1,0))
-        plot(datA = scenarioA, y = scenarioA$prev, type = 'l', xlab = "Time",
+        plot(datA = df, y = df$prev, type = 'l', xlab = "Time",
              ylab = "Prevalence", lwd = 2, ylim = c(0, 1))
          title("Prevalence of HIV")
         })
