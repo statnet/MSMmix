175cbe7fb2a8f5b757caefdc39cb09593d3070ca
kweiss2 <kevin.weiss@emory.edu>
2017-02-09 09:58:11 -0500

    Create new files - EpiModel Test and fx.R

---
+++ Shiny/fx.R
@@ -0,0 +1,45 @@
+## New module
+Qmod <- function(control, init, param) {
+    with(as.list(c(init, param)), {
+        
+        ## Dynamic Calculations ##
+        
+        # Popsize
+        N.high <- S.high + I.high
+        N.low <- S.low + I.low
+        N <- N.high + N.low
+        prev <- (I.high + I.low)/N
+        
+        # Contact rates
+        c.high <- (c.mean*N - c.low*N.low)/N.high
+        
+        # mixing matrix calculations based on Q
+        g.hh <- ((c.high*N.high) + (Q*c.low*N.low)) / ((c.high*N.high) + (c.low*N.low))
+        g.lh <- 1 - g.hh
+        g.hl <- (1 - g.hh) * ((c.high*N.high) / (c.low*N.low))
+        g.ll <- 1 - g.hl
+        
+        # prob that p is infected
+        p.high <- (g.hh*I.high/N.high) + (g.lh*I.low/N.low)
+        p.low <- (g.ll*I.low/N.low) + (g.hl*I.high/N.high)
+        
+        # lambda - force of infection
+        lambda.high <- rho.high*c.high*p.high
+        lambda.low <- rho.low*c.low*p.low
+        
+        ## Differential Equations ##
+        dS.high <- brate*prop.high*N.tot - lambda.high*S.high - muS.high*S.high 
+        dI.high <- lambda.high*S.high - muI.high*I.high
+        
+        dS.low <- brate*prop.low*N.tot - lambda.low*S.low - muS.low*S.low
+        dI.low <- lambda.low*S.low - muI.low*I.low
+        
+        
+        ## Output ##
+        list(c(dS.high, dI.high,
+               dS.low, dI.low),
+             N = N,
+             prev = prev)
+        
+    })
+}
---
+++ testEpiModel.R
@@ -0,0 +1,58 @@
+library(shiny)
+library(shinydashboard)
+library(deSolve)
+library(EpiModel)
+source("fx.R")
+
+shinyServer(function(input, output, session) {
+    
+    # Params values
+    param <- reactive({
+        param.dcm(
+            c.mean <- input$c.mean / 50,
+            c.low <- input$c.low / 50,
+            rho.high <- input$rho.high,
+            rho.low <- input$rho.low,
+            b.rate <- input$b.rate / 50,
+            muS.high <- input$muS.high / 50,
+            muS.low <- input$muS.low / 50,
+            muI.high <- input$muI.high / 50,
+            muI.low <- input$muI.low / 50,
+            Q <- input$Q
+        )
+    })
+    
+    init <- reactive({
+        init.dcm(
+            prop.high <- input$prop.high,
+            prop.low <- 1 - input$prop.high,
+            N.tot <- input$N.tot,
+            S.high = (prop.high)*(N.tot) - 1,
+            I.high = 1,
+            S.low = (prop.low)*(N.tot) - 1,
+            I.low = 1
+        )
+    })
+    
+    control <- reactive({
+        control.dcm(type = "SI",
+                    nsteps = 100 / 0.02,
+                    dt = 0.02,
+                    verbose = FALSE,
+                    odemethod = "rk4",
+                    new.mod = Qmod)
+    })
+    
+    
+    # Reactive parameters
+    ## ------------------------------------------------------------------------
+    
+    scenarioA <- data.frame(isolate(dcm(param(), init(), control())))
+    
+    output$a_Plot <- renderPlot({
+        par(mfrow = c(1,1), mgp = c(2,1,0))
+        plot(datA = scenarioA, y = scenarioA$prev, type = 'l', xlab = "Time",
+             ylab = "Prevalence", lwd = 2, ylim = c(0, 1))
+        title("Prevalence of HIV")
+    })
+})
