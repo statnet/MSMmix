d4a0b2562d343bd96fff23884b0bf58c88ca119e
smjenness <sjenness@uw.edu>
2017-03-14 11:50:01 -0400

    Fix birth rate calculation

--- Shiny/fx.R
+++ Shiny/fx.R
@@ -20,12 +20,11 @@ Qmod <- function(t, t0, parms) {
         lambda.g1 <-  rho.g1 * c.g1 * (g.11*I.g1/N.g1) + rho.g2 * c.g1 * (g.21*I.g2/N.g2)
         lambda.g2 <-  rho.g2 * c.g2 * (g.22*I.g2/N.g2) + rho.g1 * c.g2 * (g.12*I.g1/N.g1)
         
-        
         ## Differential Equations ##
-        dS.g1 <- 0.5*b.rate*N.g1 - lambda.g1*S.g1 - muS.g1*S.g1
+        dS.g1 <- b.rate*N.g1 - lambda.g1*S.g1 - muS.g1*S.g1
         dI.g1 <- lambda.g1*S.g1 - muI.g1*I.g1
 
-        dS.g2 <- 0.5*b.rate*N.g2 - lambda.g2*S.g2 - muS.g2*S.g2
+        dS.g2 <- b.rate*N.g2 - lambda.g2*S.g2 - muS.g2*S.g2
         dI.g2 <- lambda.g2*S.g2 - muI.g2*I.g2
 
 
